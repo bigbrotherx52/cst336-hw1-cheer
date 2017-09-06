344b20d812297b072cfa4e5bfa03798f2fe6f801
Harley Cheer <hcheer@csumb.edu>
2017-09-06 21:08:34 +0000

    updated CSS and index

--- index.php
+++ index.php
@@ -6,16 +6,15 @@ First Website
 and comment
 in html
 (comments can span multiple lines)
-
 -->
 
 <!-- This is the head -->
 <!-- All styles and javascript go inside the head -->
     <head>
         <meta charset = "utf-8"/>
-        <title> Harlan Cheer: Personal Website</title>
-        <link href="portfolio/css/style.css" rel="stylesheet" type="text/css"/>
-        <link href="https://fonts.googleapis.com/css?family=Krona+One+Titillium+Web:600" rel="stylesheet">
+        <title> Design Pattern: Object Pool </title>
+        <link href="dir/css/style.css" rel="stylesheet" type="text/css"/>
+        <!--<link href="https://fonts.googleapis.com/css?family=Krona+One+Titillium+Web:600" rel="stylesheet">-->
     </head>
 <!-- closing head -->
 
@@ -25,51 +24,47 @@ in html
         
         <header>
             <h1>
-                Harlan Cheer
+                Object Pool
             </h1>
+            <h2>
+                Overview
+            </h2>
         </header>
         <nav>
             <hr  />
-            <a id="currentLink" href="index.php">Home</a>
-            <a href="portfolio/about.html">About</a>
-            <a href="portfolio/contact.html">Contact</a>
-            
+            <a id="currentLink" href="index.php">Overview</a>
+            <a href="dir/problem.html">Problem</a>
+            <a href="dir/object-pool.html">Object Pool</a>
+            <a href="dir/free-list.html">Free List</a>
         </nav>
         <br/><br/>
         
         <main>
-            <figure id="me">
-                <img src = "portfolio/img/me2.jpg" alt="Picture of Harley Cheer">
-            </figure>
-            
-            <div id = "welcomeText">Hello! <br/>
-            <p>
-                This is my introduction website made for CST336!
-            </p>
-            <p>
-                Check Around!
-            </p>
-            
-            <p>
-                You Should Conact Me!
-            </p>
-            <br/><br/>
-                <em> "All we have to fear is <strong> fear itself!</strong>"</em><br/>
-                - FDR
-            
+            <div class = "content">
+                <p>
+                    Say you're playing a game, and you notice that at any given time, there are hundreds of little things appearing and vanishing in an instant, with apparently no 
+                    hang ups or loading. You think to yourself "man, this is buttery smooth, everything I make takes forever to load." Well one way they achieve this, especially on 
+                    consoles, is to use a design pattern known as Object Pool.
+                </p>
+                <p>
+                    This web page will give you a quick breakdown on Object Pool, and how you could use it to help save time and possibly keep your programs from crashing!
+                </p>
             </div>
+            
+            
+            <div class= "footnote"> For additional information on this topic, check out <a href="http://gameprogrammingpatterns.com/object-pool.html">Game Programming Patterns: Object Pool</a> 
+            for a great breakdown on Object Pools, and other design patterns</div>
         </main>
         <!-- This is the footer -->
         <!-- The footer goes inside the body but not always -->
         <footer>
             
             <hr>
-            CST 336 Internet Programming. 2017 &copy; Cheer<br/>
-            <strong>Disclaimer:</strong> The info in this webpages is not that true<br/>
-            it is used for my skoolin.
+            CST 336 Internet Programming. 2017 &copy; Harlan Cheer<br/>
+            <strong>Disclaimer:</strong> This site is for educational use<br/>
             <br/>
             <figure id ="otter">
-                <img src="portfolio/img/otter.jpg" alt = "Go Otters!"></img>
+                <img src="dir/img/otter.jpg" alt = "Go Otters!"></img>
             </figure>
         </footer>
         <!-- closing footer -->
