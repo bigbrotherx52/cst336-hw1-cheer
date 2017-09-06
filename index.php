c69085dad6efe97333377e4fda6c43161fb05514
Harley Cheer <hcheer@csumb.edu>
2017-09-06 05:21:12 +0000

    basic set up and barebones html/css

---
+++ index.php
@@ -0,0 +1,80 @@
+
+<html>
+    <!--
+
+First Website
+and comment
+in html
+(comments can span multiple lines)
+
+-->
+
+<!-- This is the head -->
+<!-- All styles and javascript go inside the head -->
+    <head>
+        <meta charset = "utf-8"/>
+        <title> Harlan Cheer: Personal Website</title>
+        <link href="portfolio/css/style.css" rel="stylesheet" type="text/css"/>
+        <link href="https://fonts.googleapis.com/css?family=Krona+One+Titillium+Web:600" rel="stylesheet">
+    </head>
+<!-- closing head -->
+
+    <!-- This is the body -->
+    <!-- This is where we place the content of our website -->
+    <body>
+        
+        <header>
+            <h1>
+                Harlan Cheer
+            </h1>
+        </header>
+        <nav>
+            <hr  />
+            <a id="currentLink" href="index.php">Home</a>
+            <a href="portfolio/about.html">About</a>
+            <a href="portfolio/contact.html">Contact</a>
+            
+        </nav>
+        <br/><br/>
+        
+        <main>
+            <figure id="me">
+                <img src = "portfolio/img/me2.jpg" alt="Picture of Harley Cheer">
+            </figure>
+            
+            <div id = "welcomeText">Hello! <br/>
+            <p>
+                This is my introduction website made for CST336!
+            </p>
+            <p>
+                Check Around!
+            </p>
+            
+            <p>
+                You Should Conact Me!
+            </p>
+            <br/><br/>
+                <em> "All we have to fear is <strong> fear itself!</strong>"</em><br/>
+                - FDR
+            
+            </div>
+        </main>
+        <!-- This is the footer -->
+        <!-- The footer goes inside the body but not always -->
+        <footer>
+            
+            <hr>
+            CST 336 Internet Programming. 2017 &copy; Cheer<br/>
+            <strong>Disclaimer:</strong> The info in this webpages is not that true<br/>
+            it is used for my skoolin.
+            <br/>
+            <figure id ="otter">
+                <img src="portfolio/img/otter.jpg" alt = "Go Otters!"></img>
+            </figure>
+        </footer>
+        <!-- closing footer -->
+        
+    </body>
+    <!-- closing body -->
+
+</html>
